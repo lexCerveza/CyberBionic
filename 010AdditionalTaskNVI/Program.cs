9fb8f6316d48f841deeb99616f9375169a37f69a
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-09-18 10:38:44 +0300

    Finish Lesson 10 (Lesson 9 skipped due it's hard stuff for me right now)

---
+++ 010AdditionalTaskNVI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _010AdditionalTaskNVI
+{
+    public class BaseClass
+    {
+        public virtual void DoStuff()
+        {
+            Console.WriteLine("DoStuff : BaseClass");
+        }
+    }
+
+    public class ChildClass : BaseClass
+    {
+        public override void DoStuff()
+        {
+            Console.WriteLine("DoStuff : ChildClass");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            BaseClass instance = new ChildClass();
+            instance.DoStuff();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 010Task1TemplateMethod/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _010Task1TemplateMethod
+{
+    public abstract class BaseAlgorithm
+    {
+        public void DoAlgorithm()
+        {
+            Step1();
+            Step2();
+            Step3();
+        }
+
+        public abstract void Step1();
+        public abstract void Step2();
+        public abstract void Step3();
+    }
+
+    public class CustomAlgorithm : BaseAlgorithm
+    {
+        public override void Step1()
+        {
+            Console.WriteLine("Step 1");
+        }
+
+        public override void Step2()
+        {
+            Console.WriteLine("Step 2");
+        }
+
+        public override void Step3()
+        {
+            Console.WriteLine("Step 3");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BaseAlgorithm algorithm = new CustomAlgorithm();
+            algorithm.DoAlgorithm();
+
+            Console.Read();
+        }
+    }
+}
