d85b0861c53a6cc6916e5ab841be288c34b32c82
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-09-22 13:54:17 +0300

    Finished Lesson 11

---
+++ 011AdditionalTaskThreadsStepByStep/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace _011AdditionalTaskThreadsStepByStep
+{
+    class Program
+    {
+        static void ThreadTask()
+        {
+            Console.WriteLine("Thread Start");
+            Console.WriteLine("Thread Finish");
+        }
+
+        static void Main()
+        {
+            var thread1 = new Thread(ThreadTask);
+            var thread2 = new Thread(ThreadTask);
+            var thread3 = new Thread(ThreadTask);
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            Console.Read();
+        }
+    }
+}
---
+++ 011Task2MultiThreadFile/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace _011Task2MultiThreadFile
+{
+    class Program
+    {
+        private static readonly object Locker = new object();
+
+        static void Thread1Task()
+        {
+            var fileContents = File.ReadAllText("File1.txt");
+
+            lock (Locker)
+            {
+                using (var fileStream = File.Open("File3.txt", FileMode.Append))
+                {
+                    var bytes = new UTF8Encoding().GetBytes(fileContents);
+                    fileStream.Write(bytes, 0, bytes.Length);
+
+                    fileStream.Close();
+                }
+            }
+        }
+
+        static void Thread2Task()
+        {
+            var fileContents = File.ReadAllText("File2.txt");
+
+            lock (Locker)
+            {
+                using (var fileStream = File.Open("File3.txt", FileMode.Append))
+                {
+                    var bytes = new UTF8Encoding().GetBytes(fileContents);
+                    fileStream.Write(bytes, 0, bytes.Length);
+
+                    fileStream.Close();
+                }
+            }
+        }
+
+        static void Main()
+        {
+            var thread1 = new Thread(Thread1Task);
+            var thread2 = new Thread(Thread2Task);
+
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+}
