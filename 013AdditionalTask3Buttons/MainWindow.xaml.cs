c576da1904ebc4b327f075f1759841676c885710
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-09-28 12:25:04 +0300

    Finished Lesson 13

---
+++ 013AdditionalTask3Buttons/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace _013AdditionalTask3Buttons
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Func<int, int, int> _addDelegate;
+        private const int X = 1;
+        private const int Y = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _addDelegate = ((x, y) => x + y);
+        }
+
+
+        private void CompleteOnClick(object sender, RoutedEventArgs e)
+        {
+            var asyncResult = _addDelegate.BeginInvoke(X, Y, null, _addDelegate);
+
+            while (!asyncResult.IsCompleted)
+            {
+                Thread.Sleep(1);
+            }
+            MessageBox.Show("Result - " + _addDelegate.EndInvoke(asyncResult));
+        }
+
+        private void EndOnClick(object sender, RoutedEventArgs e)
+        {
+            var asyncResult = _addDelegate.BeginInvoke(X, Y, null, _addDelegate);
+
+            var result = _addDelegate.EndInvoke(asyncResult);
+            MessageBox.Show("Result - " + result);
+        }
+
+        private void CallBackOnClick(object sender, RoutedEventArgs e)
+        {
+            _addDelegate.BeginInvoke(X, Y,
+                                       (asyncResult) =>
+                                       {
+                                           var asyncState = asyncResult.AsyncState as Func<int, int, int>;
+                                           if (asyncState != null)
+                                           {
+                                               var result = asyncState.EndInvoke(asyncResult);
+                                               MessageBox.Show("Result - " + result);
+                                           }
+                                       }
+                                       , _addDelegate);
+        }
+    }
+}
---
+++ 013AsyncTaskCallback/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _013AsyncTaskCallback
+{
+    class Program
+    {
+        static void Main()
+        {
+            var customDelegate = new Func<string, string>(GetHelloString);
+
+            customDelegate.BeginInvoke("Mark",
+                                       (asyncResult) =>
+                                       {
+                                           var asyncState = asyncResult.AsyncState as Func<string, string>;
+                                           if (asyncState != null)
+                                           {
+                                               var result = asyncState.EndInvoke(asyncResult);
+                                               Console.WriteLine("Callback - " + result);
+                                           }
+
+                                           Console.WriteLine("Async Task End");
+                                       }
+                                       , customDelegate);
+
+            Console.Read();
+        }
+
+        static string GetHelloString(string input)
+        {
+            return "Hello, " + input;
+        }
+    }
+}
