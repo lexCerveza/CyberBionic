0818ba5e556e92c08596fe1bd65f5c8bc0379561
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-10-02 13:25:10 +0300

    Finished Lesson 14 and 15

---
+++ 014-015AdditionalTaskPLINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace _014_015AdditionalTaskPLINQ
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result = Enumerable.Range(0, 1000000).Select(i => new Random().Next(10)).AsParallel().Where(i => (i % 2 == 1)).ToArray();
+        }
+    }
+}
---
+++ 014-015Task2AsyncAwait/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _014_015Task2AsyncAwait
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task.Run(() => Console.WriteLine("First Task started ..."));
+            Task.Run(() => Console.WriteLine("Second Task started ..."));
+
+            Console.WriteLine("Main continue executing");
+            Console.Read();
+        }
+    }
+}
