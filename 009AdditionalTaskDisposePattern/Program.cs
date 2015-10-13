2bca174b2af697ebe5622dd8356be81c83ab17f7
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-10-13 23:13:32 +0300

    Finished Lesson 9

---
+++ 009AdditionalTaskDisposePattern/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _009AdditionalTaskDisposePattern
+{
+    class DisposedClass : IDisposable
+    {
+        private bool _disposed;
+        private List<object> _objectList = new List<object>(1000000).Select(i => new object()).ToList(); 
+
+        ~DisposedClass() { }
+
+        private void CleanUp(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                // Managed resources clean up
+                _objectList.Clear();
+                _objectList = null;
+            }
+
+            // Unmanaged resources clean up
+
+            // Disposed flag turn to true
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            CleanUp(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            using (var disposedClass = new DisposedClass())
+            {
+                Console.WriteLine("Dipose method will be called in the end of this block");
+            }
+
+            Console.WriteLine("Dispose() called");
+        }
+    }
+}
---
+++ 009Task2ResourceMonitoring/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _009Task2ResourceMonitoring
+{
+    enum MemoryConsumingLevel : long
+    {
+        Low = 10000,
+        Medium = 20000,
+        High = 30000
+    }
+
+    class ResourcesMonitor
+    {
+        public MemoryConsumingLevel MemoryConsumingLevel { get; set; }
+
+        public bool IsCriticalMemoryConsuming()
+        {
+            return GC.GetTotalMemory(false) > (long) Enum.Parse(typeof(MemoryConsumingLevel), Enum.GetName(typeof(MemoryConsumingLevel), MemoryConsumingLevel));
+        } 
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var monitor = new ResourcesMonitor
+            {
+                MemoryConsumingLevel = MemoryConsumingLevel.Medium
+            };
+
+            var a = monitor.IsCriticalMemoryConsuming();
+        }
+    }
+}
