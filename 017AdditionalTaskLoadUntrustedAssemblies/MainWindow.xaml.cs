dd4a0aeb299f51be06c09dcdf6d509e77d1a6a4d
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-10-16 15:16:55 +0300

    Implemented additional task for lesson 17

---
+++ 017AdditionalTaskLoadUntrustedAssemblies/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace _017AdditionalTaskLoadUntrustedAssemblies
+{
+    public partial class MainWindow : Window
+    {
+        private const string Path = @"";
+        private const string Filter = @"(*.exe)|*.exe|(*.dll)|*.dll";
+
+        private PermissionState _state;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ExitMenuItem.Click += (sender, args) =>
+            {
+                Close();
+            };
+
+            AssemblyChooseButton.Click += (sender, args) =>
+            {
+                _state = (((ComboBoxItem) ComboBox.SelectedValue).Content.ToString() == "None") ? 
+                      PermissionState.None
+                    : PermissionState.Unrestricted;
+
+                var openFileDialog = new OpenFileDialog
+                {
+                    InitialDirectory = Path,
+                    Filter = Filter
+                };
+
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    var asmPath = openFileDialog.SafeFileName.Split('.')[0];
+
+                    var newDomain = AppDomain.CreateDomain(asmPath);
+                    var permissionSet = new PermissionSet(_state);
+                    permissionSet.AddPermission(new SecurityPermission(SecurityPermissionFlag.AllFlags));
+
+                    newDomain.Load(new AssemblyName(asmPath));
+
+                    MessageBox.Show(string.Format("{0} loaded", asmPath));
+                }
+            };
+        }
+    }
+}
