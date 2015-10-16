7368e79e5f038d29772501b6a1fd7d8dadb2ec48
Alex Kraevyy <clockworkalexorange@gmail.com>
2015-10-16 15:27:07 +0300

    Finished Lesson 17 and courses.

---
+++ 017Task2ServiceInstall/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _017Task2ServiceInstall
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Service installing ...");
+            Process.Start(@"C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil.exe", "WindowsServices.exe");
+            Console.WriteLine("Service install done ...");
+            Console.Read();
+        }
+    }
+}
